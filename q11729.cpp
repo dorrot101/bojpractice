35ec83636d61eccabcf1706190ef08257f8591dd
dorrot <kimhyunwoo101@gmail.com>
2021-04-05 15:37:44 +0900

    correct: q11729

--- q11729.cpp
+++ q11729.cpp
@@ -2,21 +2,26 @@
 
 #include<iostream>
 
-void hanoi(int from, int to, int num, int count) {
-    ++count;
+int getnum(int n) {
+    if (n == 1) return 1;
+    return 2 * getnum(n - 1) + 1;
+}
+
+void hanoi(int from, int to, int num) {
     if (num == 1) {
-        std::cout << count << '\n';
-        std::cout << from << to << '\n';
+        std::cout << from << ' ' << to << '\n';
         return;
     }
-    hanoi(from, 6 - to - from, --num, count);
-    std::cout << from << to << '\n';
-    hanoi(6 - to - from, to, num, count);
+    hanoi(from, 6 - to - from, --num);
+    std::cout << from << ' ' << to << '\n';
+    hanoi(6 - to - from, to, num);
 }
 
 int main() {
-    int n, count = 0;
+    int n;
+    int count = 0;
     std::cin >> n;
-    hanoi(1, 3, n, count);
+    std::cout<<getnum(n)<<'\n';
+    hanoi(1, 3, n);
     return 0;
 }
