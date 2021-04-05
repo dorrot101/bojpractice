9b563d961134d78462c7ff79db8f444d9aebb291
dorrot <kimhyunwoo101@gmail.com>
2021-04-05 15:24:02 +0900

    1change q11729

--- q11729.cpp
+++ q11729.cpp
@@ -2,9 +2,21 @@
 
 #include<iostream>
 
+void hanoi(int from, int to, int num, int count) {
+    ++count;
+    if (num == 1) {
+        std::cout << count << '\n';
+        std::cout << from << to << '\n';
+        return;
+    }
+    hanoi(from, 6 - to - from, --num, count);
+    std::cout << from << to << '\n';
+    hanoi(6 - to - from, to, num, count);
+}
 
-
-int main(){
-
+int main() {
+    int n, count = 0;
+    std::cin >> n;
+    hanoi(1, 3, n, count);
     return 0;
 }
